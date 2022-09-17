a4c4482ad3bc3addbce73ecc6e508ee4500b4ca2
Adrian Twarog <adrian@moshimoshi.marketing>
2022-09-17 11:59:07 +0800

    final updates

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// create a new express applications with cors enabled that will be used to handle the requests for a pricing calculator, the main endpoint will be to test that you can acceess the json data, and the /calc endpoint will recieve data and return a price.
-
 var express = require('express');
 var app = express();
 var cors = require('cors');
@@ -7,20 +5,58 @@ var cors = require('cors');
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.json({
-		success: true,
+	res.json({ 
+		success: true, 
 		message: "Pricing Calc API is running",
 	})
 })
 
 app.get('/calc', (req, res) => {
-	var { hourlyRate, hoursWorked } = req.query;
-	var price = hourlyRate * hoursWorked;
+	var { pricing, timeTodo, websiteSize, hourlyRate } = req.query;
+
+	// Validate timeTodo
+	if(timeTodo === "10-20"){
+		timeTodo = 15;
+	} else if(timeTodo === "5-10"){
+		timeTodo = 7.5;
+	} else if(timeTodo === "2-4"){
+		timeTodo = 3;
+	} else {
+		timeTodo = 15;
+	}
+
+	// Validate websiteSize
+	if(websiteSize === "Single Page"){
+		websiteSize = 1;
+	} else if(websiteSize === "​3-5 Pages"){
+		websiteSize = 4.5;
+	} else if(websiteSize === "​15-45 Pages"){
+		websiteSize = 25;
+	} else if(websiteSize === "​+50 Pages"){
+		websiteSize = 50;
+	} else {
+		websiteSize = 1;
+	}
+
+	// Validate websiteSize
+	if(hourlyRate === "​$25 per hour"){
+		hourlyRate = 25;
+	} else if(hourlyRate === "​$50 per hour"){
+		hourlyRate = 50;
+	} else if(hourlyRate === "​$100 per hour"){
+		hourlyRate = 100;
+	} else if(hourlyRate === "​+$150 per hour"){
+		hourlyRate = 150;
+	} else {
+		hourlyRate = 25;
+	}
+	let price = timeTodo*websiteSize*hourlyRate
+	console.log(pricing,timeTodo,websiteSize, hourlyRate, price);
 	res.json({
 		success: true,
 		price,
 	})
-})
+})	
 
 app.listen(3000, () => {
 	console.log("Pricing Calc API is running on port 3000");
